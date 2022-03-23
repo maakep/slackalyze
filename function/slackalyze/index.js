48a0800621a58a62868064a18112b07d25ef67e4
Maakep <schon.fredrik@gmail.com>
2022-03-23 13:29:10 +0100

    test flow with code changes

--- function/slackalyze/index.js
+++ function/slackalyze/index.js
@@ -1,4 +1,4 @@
 exports.analyze = (req, res) => {
-  let message = req.query.message || req.body.message || "Hello World!";
+  let message = req.query.message || req.body.message || "Hi World!";
   res.status(200).send(message);
 };
